de08bfd592b1c5e88dbf680589143da63beebfe0
mansiey <mansi46rd@gmail.com>
2026-05-29 23:16:34 +0530

    add basic skeleton for list

---
+++ 03-Color-Palette/script.js
@@ -0,0 +1,9 @@
+const formatSelect = document.getElementById("format");
+const toneSelect = document.getElementById("tone");
+const generateBtn = document.getElementById("generateBtn");
+
+function generatePalette() {
+    
+}
+
+generateBtn.addEventListener('click', generatePalette);
